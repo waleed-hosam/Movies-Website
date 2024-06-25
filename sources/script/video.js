1927a184bc89626ae61781550931590e6b54ff88
Waleed Hosam <152779925+waleed-hosam@users.noreply.github.com>
2024-06-25 14:40:16 +0300

    Update video.js

--- sources/script/video.js
+++ sources/script/video.js
@@ -1,1 +1,34 @@
-
+let videos = document.querySelectorAll(".videos video")
+let links = document.querySelectorAll(".box a")
+let active_video = document.querySelector(".videos .active")
+let videoDiv = document.querySelector('.videos')
+let closeBtn = document.querySelector(".fa-xmark")
+links.forEach((link, i) => {
+    link.onclick = () => {
+        videoDiv.classList.remove("hide")
+        closeBtn.classList.remove("hide")
+        videos[i].classList.add("active")
+        videos[i].play()
+        document.querySelector("html").style.overflowY = "hidden"
+    }
+})
+closeBtn.onclick = () => {
+    videos.forEach((video) => {
+        video.classList.remove("active")
+        video.pause()
+    })
+    closeBtn.classList.add("hide")
+    videoDiv.classList.add("hide")
+    document.querySelector("html").style.overflowY = "auto"
+}
+window.addEventListener("click", e => {
+    if (e.target == videoDiv && e.target != active_video) {
+        videos.forEach((video) => {
+            video.classList.remove("active")
+            video.pause()
+        })
+        closeBtn.classList.add("hide")
+        videoDiv.classList.add("hide")
+        document.querySelector("html").style.overflowY = "auto"
+    }
+})
